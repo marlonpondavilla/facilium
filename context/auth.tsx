48eaee94f120ad19bcf08e872bd0a7f195978962
MarlonDev <marlonpondavilla@dev.com>
2025-06-26 19:03:47 +0800

    chore: update dependencies and devDependencies in package.json
    - Added new dependencies: class-variance-authority, clsx, firebase, firebase-admin, lucide-react, tailwind-merge
    - Updated devDependencies: @eslint/eslintrc, @tailwindcss/postcss, tailwindcss, tw-animate-css, typescript

---
+++ context/auth.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged, User, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import {auth} from "@/firebase/client"
+
+type AuthContextType = {
+  user: User | null;
+  loading: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  signup: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({children}: {
+  children: React.ReactNode
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const login = async (email:string, password:string) => {
+    await signInWithEmailAndPassword(auth, email, password);
+  }
+
+  const signup = async (email: string, password:string) => {
+    await createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  const logout = async () => {
+    await signOut(auth);
+  }
+
+  return (
+    <AuthContext.Provider value={{user, loading, login, signup, logout}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
